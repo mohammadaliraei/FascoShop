7134e016e9ce620cc3c79b2cc6ae1490a6f744c7
mohammadaliraei <mobham.des@gmail.com>
2024-03-27 15:21:48 +0330

    fix bug

--- src/componenets/dealOfTheMonth/SwiperProducts.tsx
+++ src/componenets/dealOfTheMonth/SwiperProducts.tsx
@@ -4,13 +4,12 @@ import { Carousel } from "flowbite-react";
 const SwiperProducts = () => {
   // const [currentIndex, setCurrentIndex] = useState(0);
 
-  // const dataImages = [
-  //   images.GrilOne,
-  //   images.GrilTwo,
-  //   images.GrilThree,
-  //   images.GrilFour,
-  // ];
-  const dataImages2 = [images.R2, images.R3, images.R4, images.R1];
+  const dataImages = [
+    images.GrilOne,
+    images.GrilTwo,
+    images.GrilThree,
+    images.GrilFour,
+  ];
 
   // const prevIndex = () => {
   //   setCurrentIndex((prevIndex) =>
@@ -28,7 +27,7 @@ const SwiperProducts = () => {
   return (
     <div className="flex flex-row h-full w-full gap-4 ">
       <Carousel>
-        {dataImages2.map((images, i) => (
+        {dataImages.map((images, i) => (
           <img
             key={i}
             src={images}
